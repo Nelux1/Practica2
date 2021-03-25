e61a654ab5d424d46bf24e599d4f4b03dfeb62bd
Marcos Suarez <negrosuarez22@gmail.com>
2021-03-25 17:11:33 -0300

    ejercicio 2 practica 2

---
+++ README2.py
@@ -0,0 +1,87 @@
+# <img alt="NumPy" src="/branding/logo/primary/numpylogo.svg" height="60">
+
+texto='''NumPy is the fundamental package needed for scientific computing with Python.
+
+- **Website:** https://www.numpy.org
+- **Documentation:** https://numpy.org/doc
+- **Mailing list:** https://mail.python.org/mailman/listinfo/numpy-discussion
+- **Source code:** https://github.com/numpy/numpy
+- **Contributing:** https://www.numpy.org/devdocs/dev/index.html
+- **Bug reports:** https://github.com/numpy/numpy/issues
+- **Report a security vulnerability:** https://tidelift.com/docs/security
+
+It provides:
+
+- a powerful N-dimensional array object
+- sophisticated (broadcasting) functions
+- tools for integrating C/C++ and Fortran code
+- useful linear algebra, Fourier transform, and random number capabilities
+
+Testing:
+
+NumPy requires `pytest`.  Tests can then be run after installation with:
+
+    python -c 'import numpy; numpy.test()'
+
+
+Call for Contributions
+----------------------
+
+The NumPy project welcomes your expertise and enthusiasm!
+
+Small improvements or fixes are always appreciated; issues labeled as "good
+first issue" may be a good starting point. If you are considering larger
+contributions to the source code, please contact us through the [mailing
+list](https://mail.python.org/mailman/listinfo/numpy-discussion) first. 
+
+Writing code isn’t the only way to contribute to NumPy. You can also: 
+- review pull requests
+- triage issues
+- develop tutorials, presentations, and other educational materials
+- maintain and improve [our website](https://github.com/numpy/numpy.org)
+- develop graphic design for our brand assets and promotional materials
+- translate website content
+- help with outreach and onboard new contributors
+- write grant proposals and help with other fundraising efforts
+
+If you’re unsure where to start or how your skills fit in, reach out! You can
+ask on the mailing list or here, on GitHub, by opening a new issue or leaving a
+comment on a relevant issue that is already open.
+
+Our preferred channels of communication are all public, but if you’d like to
+speak to us in private first, contact our community coordinators at
+numpy-team@googlegroups.com or on Slack (write numpy-team@googlegroups.com for
+an invite).
+
+We also have a biweekly community call, details of which are announced on the
+mailing list. You are very welcome to join. 
+
+If you are new to contributing to open source, [this
+guide](https://opensource.guide/how-to-contribute/) helps explain why, what,
+and how to successfully get involved.'''
+
+sacar= "@*,;:.)([]}{/-\n!\"'"
+max= -1
+for caracter in sacar:
+    texto= texto.replace(caracter," ")
+for caracter in sacar:
+    texto= texto.replace("  "," ")    
+texto= texto.lower()
+palabras= texto.split(" ")
+frecuencias= {}
+for p in palabras:
+ if p in frecuencias:
+         frecuencias[p] += 1
+ else:
+         frecuencias[p] = 1
+for p in frecuencias:
+    frecuencia=frecuencias[p]            
+    print (f"palabra: {p} {frecuencia}")
+for p in frecuencias:
+    frecuencia= frecuencias[p]    
+    if max < frecuencia :
+       max= frecuencia
+       PalMax={p}
+       print (f'la palabra que aparece mayor cantidad de veces es {p} con {frecuencia} cantidad de veces') 
+    
+       
